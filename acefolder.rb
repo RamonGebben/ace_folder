1290d92062fa7a8283ee83bd2dea4081f3ff5db3
Ralf Nieuwenhuijsen <r.nieuwenhuijsen@synovite.nl>
2014-11-08 18:30:34 +0100

    * more cleanup

--- acefolder.rb
+++ acefolder.rb
@@ -2,18 +2,19 @@ require 'sinatra'
 require 'json'
 
 # set to whichever folder you want to edit
-set :acefolder, 'public'
+set :acefolder, 'public/'
+set :file_type_whitelist, "css js html md".split(' ')
 
 use Rack::Auth::Basic, "Restricted Area" do |username, password|
   username == 'demo' and password == 'scaffold'
 end
 
 get '/' do
-  File.read( "index.html" )
+  File.read( "/home/ralf/Projects/ace_folder/public/index.html" )
 end
 
 get '/structure' do
-  Dir.glob( settings.acefolder + "/**/*").reject{ |fn| File.directory?(fn) }.collect{ |c| settings.acefolder + '/', "") }.to_json
+  Dir.glob( settings.acefolder + "**/*").reject{ |fn| File.directory?(fn) }.collect{ |c| c.gsub( settings.acefolder, "" ) }.to_json
 end
 
 get '/file/*' do
--- public/together.conf.js
+++ public/together.conf.js
@@ -1,5 +1,1 @@
 var TogetherJSConfig_suppressJoinConfirmation = true;
-var TogetherJSConfig_autoStart = true;
-var TogetherJSConfig_findRoom = "nextgen";
-var TogetherJSConfig_siteName = "ACE-Folder456";
-var TogetherJSConfig_dontShowClicks = true;
