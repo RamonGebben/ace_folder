16804237fafc4300b087a1023e20cddbfa2640b2
Ramon Gebben <ramon@ra-ge.net>
2014-11-09 22:03:35 +0100

    redone folders

--- demo/app/app.rb
+++ demo/app/app.rb
@@ -1,30 +1,7 @@
-require 'sinatra'
-require 'json'
+.pizza {
+    box-shadow: 0 0 15px;
+}
 
-# set to whichever folder you want to edit
-set :acefolder, 'demo/'
-set :file_type_whitelist, "css js html md".split(' ')
-
-use Rack::Auth::Basic, "Restricted Area" do |username, password|
-  username == 'demo' and password == 'scaffold'
-end
-
-def legal_files
-end
-
-get '/' do
-  File.read( "public/index.html" )
-end
-
-get '/structure' do
-  Dir.glob( settings.acefolder + "**/*").reject{ |fn| File.directory?(fn) }.collect{ |c| c.gsub( settings.acefolder, "" ) }.to_json
-end
-
-get '/file/*' do
-  File.read( settings.acefolder + params[:splat].first.gsub("..","") )
-end
-
-put '/file/*' do
-  File.write( settings.acefolder + params[:splat].first.gsub("..",""), request.body.read.to_s )
-  "OK"
-end
+#hightlight{
+    text-align: center;
+}
